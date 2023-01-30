1c1e8535024f886da5bb018e749753fac6d84776
Andre Jarboe <andrejarboe@gmail.com>
2023-01-30 15:17:21 -0500

    -- dispaly avg price of red items

--- Clothing_Store_db.sql
+++ Clothing_Store_db.sql
@@ -19,7 +19,7 @@ Insert INTO store VALUES (6, "undershirt", "black", 64, 4);
 Insert INTO store VALUES (7, "stripped jacket", "blue", 7, 5);
 Insert INTO store VALUES (8, "Texas hat", "red", 86, 234);
 Insert INTO store VALUES (9, "Texas pants", "blue", 32, 44);
-Insert INTO store VALUES (10, "clemson shirt", "black", 7, 4325);
+Insert INTO store VALUES (10, "clemson shirt", "black", 7, 25);
 Insert INTO store VALUES (11, "vest", "black", 34, 34);
 Insert INTO store VALUES (12, "heated vest", "black", 7, 45);
 Insert INTO store VALUES (13, "shoes", "white", 12, 478);
@@ -29,3 +29,12 @@ Insert INTO store VALUES (15, "boots", "black", 39, 56);
 -- display the database ordered by price. 
 SELECT * FROM store 
 ORDER BY Price DESC;
+
+-- dispaly avg price of red items
+SELECT 
+    AVG(price) "Avg price of Red items"
+FROM 
+    store
+WHERE 
+    color = "red";
+
